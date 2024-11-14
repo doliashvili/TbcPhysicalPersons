adb96587b817ee3d7aac2a95e4f2b700ca1b3673
ლევან დოლიაშვილი <doliashvili.levan@itstep.ge>
2024-11-14 17:14:08 +0400

    two bad answers

---
+++ PhysicalPersonsDirectory/CodeInterview/Examples.cs
@@ -0,0 +1,50 @@
+﻿namespace CodeInterview
+{
+    internal class Examples
+    {
+        public Task Example1()//რა დაიბეჭდება ამდროს?
+        {
+            try
+            {
+                Console.WriteLine("A");
+                DoSomething();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("D");
+            }
+
+            async Task DoSomething()
+            {
+                Console.WriteLine("B");
+                await Task.Delay(500);
+                throw new Exception("Test");
+                Console.WriteLine("C");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task Example2()//რა დაიბეჭდება ამდროს?
+        {
+            try
+            {
+                Console.WriteLine("A");
+                DoSomething();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("D");
+            }
+
+            async Task DoSomething()
+            {
+                Console.WriteLine("B");
+                await Task.Delay(500);
+                Console.WriteLine("C");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ PhysicalPersonsDirectory/CodeInterview/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeInterview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
